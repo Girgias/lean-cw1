006ebc4e27e1a150ae24e3b5ecbd606bd114e7de
George Peter Banyard <girgias@php.net>
2022-02-01 16:52:01 +0000

    More stuff

--- src/kernel-normal-subgroup.lean
+++ src/kernel-normal-subgroup.lean
@@ -1,29 +1,30 @@
 import tactic
 import group_theory.subgroup.basic
+import data.set.basic
 
 namespace my_kernel_norm_sub
 
-variables {G J : Type} [group G] [group J]
-variable {H : subgroup G}
-variable {I : subgroup J}
-variable {φ : H →* I}
+variables {G H : Type} [group G] [group H]
+variable {I : subgroup H}
+variable {φ : G →* H}
 
-lemma x_in_kernel_is_identity {k : H}(hk : k ∈ φ.ker) : φ(k) = 1 :=
+lemma map_mul_2 {x y : G} : φ(x * y * x⁻¹) = φ(x) * φ(y) * φ(x)⁻¹ :=
+begin
+    -- rewrite twice to get an obvious identiy
+  rw map_mul,
+  rw map_mul,
+end
+
+lemma x_in_kernel_is_identity {k : G}(hk : k ∈ φ.ker) : φ(k) = 1 :=
 begin
   exact hk,
 end
 
 lemma conjugating_k_in_kernel_by_x_is_identity
-  {k : H}(hk : k ∈ φ.ker){x : H} : φ(x * k * x⁻¹) = 1 :=
+  {k x : G}(hk : k ∈ φ.ker) : φ(x * k * x⁻¹) = 1 :=
 begin
-  -- multiplication 
-  have hhom : φ(x * k * x⁻¹) = φ(x) * φ(k) * φ(x⁻¹), {
-    -- rewrite twice to get an obvious identiy
-    rw map_mul,
-    rw map_mul,
-  },
   -- rewrite our hypothethis to expand to multiplication by function
-  rw hhom,
+  rw map_mul_2,
   -- kernel element goes to the identity
   rw x_in_kernel_is_identity hk,
   -- Remove the multiplication by 1
@@ -37,19 +38,53 @@ begin
 end
 
 lemma conjugating_kernel_by_x_is_in_kernel
-  {k : H}(hk : k ∈ φ.ker){x : H} : x * k * x⁻¹ ∈ φ.ker :=
+  {k : G}(hk : k ∈ φ.ker){x : G} : x * k * x⁻¹ ∈ φ.ker :=
 begin
     apply conjugating_k_in_kernel_by_x_is_identity,
     exact hk,
 end
 
-theorem kernel_is_normal_subgroup_of_domain (φ : H →* I) : subgroup.normal (φ.ker) :=
+theorem kernel_is_normal_subgroup_of_domain (φ : G →* H) : subgroup.normal (φ.ker) :=
 begin
-  -- TODO Look up what refine does
-  refine {conj_mem := _},
+  -- Change the goal to be the hypothethis of what a normal group is
+  apply subgroup.normal.mk,
   apply conjugating_kernel_by_x_is_in_kernel,
 end
 
 -- Proof wiki: https://proofwiki.org/wiki/Preimage_of_Normal_Subgroup_of_Quotient_Group_under_Quotient_Epimorphism_is_Normal
+
+#check subgroup.normal.mk
+#check I.comap φ
+
+lemma foo (φ : G →* H) {x y : subgroup.comap φ I} :
+  φ(x*y*x⁻¹) =  φ(x) * φ(y) * φ(x)⁻¹ :=
+begin
+  exact map_mul_2,
+end
+
+lemma preimage_exist (φ : G →* H) {g : G}{h : H} {hg: subgroup.comap φ I} :
+  φ(g) = h :=
+begin
+  
+end
+
+lemma bar (φ : G →* H) {i j : H} {x y : subgroup.comap φ I} :
+  φ(x*y*x⁻¹) =  φ(x) * φ(y) * φ(x)⁻¹ :=
+begin
+  exact map_mul_2,
+end
+
+theorem preimage_of_normal_subgroup_is_normal
+  (φ : G →* H) (hn : subgroup.normal(I)) : subgroup.normal (subgroup.comap φ I) :=
+begin
+  -- Change the goal to be the hypothethis of what a normal group is
+  apply subgroup.normal.mk,
+  intro hg,
+  intro hpreim,
+  rw foo,
+  sorry,
+end
+
+-- example (h ; S.normal ) : (S.comap f).normal
 
 end my_kernel_norm_sub
