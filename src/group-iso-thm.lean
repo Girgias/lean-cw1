372342d576605c86d9673f207d94d7bc0cfee01d
George Peter Banyard <girgias@php.net>
2022-03-04 00:47:39 +0000

    finish cw2

--- src/group-iso-thm.lean
+++ src/group-iso-thm.lean
@@ -1,170 +1,100 @@
 import tactic
-import data.quot
 import group_theory.quotient_group
 import group_theory.subgroup.basic
 
--- Proof wiki link: https://proofwiki.org/wiki/Isomorphism_Theorems
-
 namespace my_group_iso
 
-variables {G H : Type} [group G] [group H]
---variable {φ : G →* H}
+variables {G : Type} [group G]
 
---def K := φ.ker
-
-variables {S N T : subgroup G} [subgroup.normal N]
-
-/-
-def first_iso {φ : G →* H}{I : subgroup H} : G⧸φ.ker ≃* I :=
-{ to_fun := _,
-  inv_fun := _,
-  left_inv := _,
-  right_inv := _,
-  map_mul' := _ }
-
--/
-
---def S ⊓ N : subgroup G
-
--- Given by Kevin try to understand def
- --S/S∩N ≅ SN/N :=
-#check subgroup.comap
--- S ⊔ N is the smallest subgroup of G containing S and N.
--- Which corresponds to SN
-#check S ⊔ N
-
-/- i is the natural homomorphism S → (SN) -/
-example {S N : subgroup G} [N.normal] : S →* (S ⊔ N : subgroup G) :=
-begin
-  refine subgroup.inclusion _,
-  intro s,
-  intro hs,
-  exact subgroup.mem_sup_left hs,
-end
-
-#check quotient_group.mk' ((S ⊓ N).comap S.subtype)
-
-#check S⧸((S ⊓ N).comap S.subtype)
-
-def i {G} [group G] {S N : subgroup G} [N.normal] :
+/-- f is the natural embeding of S in SN -/
+def f {G : Type} [group G] {S N : subgroup G} [N.normal] :
   S →* (S ⊔ N : subgroup G) :=
     subgroup.inclusion le_sup_left
 
-#check monoid_hom.comp
-
-def i2 {G} [group G] (S N : subgroup G) [N.normal] :
+/-- g is the map from S to SN⧸N -/
+def g {G : Type} [group G] (S N : subgroup G) [N.normal] :
   S →* (S ⊔ N : subgroup G)⧸(N.comap (S ⊔ N).subtype) :=
-begin
-  apply monoid_hom.comp (quotient_group.mk' (N.comap (S ⊔ N).subtype)),
-  exact i,
-end
+    -- Composition of the map f: S → SN with the quotient homomorphism SN → SN/N
+    monoid_hom.comp (quotient_group.mk' (N.comap (S ⊔ N).subtype)) f
 
---#check (S ⧸ (N.comap S.subtype)).lift i2
-#check quotient_group.lift (N.comap S.subtype) (i2 S N)
-
-lemma i2_is_surjective : function.surjective (i2 S N) :=
+/-- The group homomorphism g: S → SN⧸N is surjective
+  The proof is based on the one from mathlib -/
+lemma g_is_surjective {S N : subgroup G} [N.normal] : function.surjective (g S N) :=
 begin
-  -- rewrite def
-  --rw function.surjective,
-  -- deconstruct ∀ to y is an element of the group G and the hyp as to why it's in SN
+  rw function.surjective,
+  -- deconstruct ∀
+  -- · y : G
+  -- · hypothethis y ∈ SN
   rintro ⟨y, (hy : y ∈ ↑(S ⊔ N))⟩,
-  -- change y ∈ ↑(S ⊔ N) to y ∈ ↑S*↑N
+  -- As N normal, y ∈ (S ⊔ N) means y ∈ S*N
   rw subgroup.mul_normal S N at hy,
+  -- Deconstruct y ∈ S*N
+  -- s,n : G such that s ∈ S and n ∈ N
+  -- s*n = y
+  -- rfl changes instances of y to s*n
   rcases hy with ⟨s, n, hs, hn, rfl⟩,
-  -- Why need the square brackets?
-  --use [s, hs],
+  -- 
   use s,
   exact hs,
-  -- not sure what this does
+  -- Change to the equivalence relation of the quotient
   apply quotient.eq.mpr,
   change s⁻¹ * (s * n) ∈ N,
+  -- Rebracket
   rw ← mul_assoc,
+  -- Cancel inverses
   rw inv_mul_self,
+  -- simplify multiplication by identity
   rw one_mul,
   exact hn,
 end
 
---
---  (S⧸((S ⊓ N).comap S.subtype)) →* (S ⊔ N : subgroup G)⧸(N.comap (S ⊔ N).subtype) :=
-def i3 {G} [group G] (S N : subgroup G) [N.normal] :
-  (S ⧸ (N.comap S.subtype)) →* (S ⊔ N : subgroup G)⧸(N.comap (S ⊔ N).subtype) :=
+/- Proof that the kernel of g is S∩N -/
+lemma g_ker_eq_S_intersect_N (S N : subgroup G) [N.normal] :
+  ((g S N).ker : subgroup S) = (N.comap S.subtype) :=
 begin
-  apply quotient_group.lift (N.comap S.subtype) (i2 S N),
-  intro s,
-  intro hs,
-  rw subgroup.mem_comap at hs,
-  rw subgroup.coe_subtype at hs,
-  dsimp [i2],
-  rw quotient_group.eq_one_iff,
-  rw subgroup.mem_comap,
-  rw subgroup.coe_subtype,
-  exact hs,
-end
-
-#check quotient_group.quotient_ker_equiv_of_surjective (i3 S N)
-
-lemma i3_is_bijective : function.bijective (i3 S N) :=
-begin
-  --refine function.bijective_iff_has_inverse.mpr _,
-  rw function.bijective,
+  ext,
   split, {
-    -- proof function is injective
-    rw function.injective,
-    intros x y,
-    dsimp[i3],
-    intro h,
-    --rw subgroup.mem_comap at h,
-    --rw subgroup.coe_subtype at h,
-    apply quotient.induction_on₂ x y,
-    sorry,
+    -- ker(g) ⊆ S∩N
+    simp,
+    -- x ∈ ker(g)
+    intro hxk,
+    -- Being in the kernel is definitionaly equal to sending an element to the identity
+    change (g S N) x = 1 at hxk,
+    dsimp [g] at hxk,
+    simp at hxk,
+    exact hxk,
   }, {
-    -- proof function is surjective
-    rw function.surjective,
-    --rintro ⟨y, (hy : y ∈ (S ⊔ N : subgroup G))⟩,
-    --have hys : y ∈ S, 
-    intro y,
-    dsimp[i3],
-    sorry,
-  },
+    -- S∩N ⊆ ker(g)
+    simp,
+    -- hx : x ∈ N
+    intro hx,
+    -- Being in the kernel is definitionaly equal to sending an element to the identity
+    change (g S N) x = 1,
+    dsimp [g],
+    simp,
+    exact hx,
+  }
 end
 
-def second_iso (S N : subgroup G) [N.normal]:
+/--!
+ - # The second isomorphism theorem for groups
+ - If S, N ≤ G, and N is normal, then S⧸(S∩N) ≅ (SN)⧸N
+ -/
+noncomputable def second_iso (S N : subgroup G) [N.normal]:
   S ⧸ (N.comap S.subtype) ≃* (S ⊔ N : subgroup G) ⧸ (N.comap (S ⊔ N).subtype) :=
-{
-  to_fun := (i3 S N),
-  inv_fun := _,
-  left_inv := _,
-  right_inv := _,
-  map_mul' := _
-}
-
-#check quotient_group.map (N.comap S.subtype) (N.comap (S⊔N).subtype) i
-
-def i4 {G} [group G] {S N : subgroup G} [N.normal] :
--- .comap S.subtype changes it to be a subgroup of G to be a subgroup of S
-  S⧸((S ⊓ N).comap S.subtype) →* (S ⊔ N : subgroup G)⧸(N.comap (S ⊔ N).subtype) :=
-begin sorry end
-
-
-#check i '' ↑S
-#check i ⁻¹' ↑(S ⊔ N)
-
-def f {G} [group G] {S: subgroup G} : S →* G :=
 begin
-  exact S.subtype,
-end
-def g {G} [group G] {N: subgroup G} [N.normal] : N →* G :=
-begin
-  exact N.subtype,
-end
-def h {G} [group G] {S N : subgroup G} [N.normal] : (S ⊔ N : subgroup G) →* G :=
-begin
-  refine f,
+  -- Get the isomorphism via the 1st isomorphism theorem where the LHS
+  -- is quotiented by the kernel of the surjective homomorphism
+  let e1 := quotient_group.quotient_ker_equiv_of_surjective (g S N) g_is_surjective,
+  -- Claim that S⧸(S∩N) ≅ S⧸(ker(g))
+  have h : ↥S ⧸ subgroup.comap S.subtype N ≃* ↥S ⧸ (g S N).ker,
+  let e2 := quotient_group.equiv_quotient_of_eq (g_ker_eq_S_intersect_N S N),
+  exact e2.symm,
+  -- Transitivity of isomorphisms
+  exact mul_equiv.trans h e1,
 end
 
-#check S.comap S.subtype
-
-theorem third_iso_1 {S N : subgroup G} [S.normal] [N.normal] (h: N ≤ S) :
-  subgroup.normal ((S.comap S.subtype)⧸(N.comap S.subtype)) := sorry
+variables {S N : subgroup G} [subgroup.normal N]
+#check function.surjective (g S N)
 
 end my_group_iso
